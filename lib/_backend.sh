2e849b221583a65691385b17c1566394de025e57
PHX Digital Media <72950126+xpsnetworks@users.noreply.github.com>
2023-07-09 17:51:54 -0300

    Update _backend.sh

--- lib/_backend.sh
+++ lib/_backend.sh
@@ -214,7 +214,7 @@ backend_start_pm2() {
   sudo su - deploy <<EOF
   cd /home/deploy/${instancia_add}/backend
   sudo pm2 start dist/server.js --name ${instancia_add}-backend
-  pm2 reload ${instancia_add}-backend --max-memory-restart 300M --cron-restart="0 6 * * *"
+  sudo pm2 reload ${instancia_add}-backend --max-memory-restart 300M --cron-restart="0 6 * * *"
   sudo pm2 save --force
 EOF
 
